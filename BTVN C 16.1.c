7e89b70184d0d35dec08ec4fe3c276a713f09a3a
Leducanh30112006 <le1duc2anh3@gmail.com>
2024-12-16 16:04:07 +0700

    Update BTVN C 16.1.c

--- BTVN C 16.1.c
+++ BTVN C 16.1.c
@@ -3,10 +3,10 @@
 int main() {
     int n = 10;
     int *ptr = &n;
-    printf("Gia tri cua bien var: %d\n", var);
-    printf("Dia chi cua bien var: %p\n", (void*)&n);
-    printf("Gia tri cua bien var (thong qua con tro): %d\n", *ptr);
-    printf("Dia chi cua bien var (thong qua con tro): %p\n", (void*)ptr);
+    printf("Gia tri cua bien : %d\n", ptr);
+    printf("Dia chi cua bien : %p\n", (void*)&n);
+    printf("Gia tri cua bien thong qua con tro: %d\n", *ptr);
+    printf("Dia chi cua bien thong qua con tro: %p\n", (void*)ptr);
 
     return 0;
 }
